aa2865054c24ea9376edbf70877a8abc112f08d4
Jonathan Dahan <git@jonathan.is>
2023-03-28 14:31:23 -0400

    Small cleanup

--- fetch.mjs
+++ fetch.mjs
@@ -1,26 +1,32 @@
 import { ENDPOINTS, STREAMS } from './constants.mjs'
 import { get } from 'datagarrison'
-import { encode } from 'querystring'
+
+const parseJson = (response) => {
+  if (response.ok) return response.json()
+  throw new Error(`Request rejected with status ${response.status}`)
+}
+
+const throwJsonErrors = (json) => {
+  if (json.error && json.error.message) throw new Error(json.error.message)
+  return json
+}
+
+const mapTimeDataToGMT = (json) => ({
+  ...json,
+  source,
+  data: json.data?.map(datum => ({ ...datum, t: `${datum.t} GMT` }))
+})
 
 export const fetchNoaaData = () => {
   console.log('Fetching NOAA data')
   const { url, query } = ENDPOINTS.noaaCurrent
-  const source = `${url}?${encode(query)}`
+  const source = new URL(url)
+  url.search = new URLSearchParams(query)
 
   return fetch(source, { method: 'GET' })
-    .then(response => {
-      if (response.ok) return response.json()
-      throw new Error(`Request rejected with status ${response.status}`)
-    })
-    .then(json => {
-      if (json.error && json.error.message) throw new Error(json.error.message)
-      return json
-    })
-    .then(json => ({
-      ...json,
-      source,
-      data: json.data?.map(datum => ({ ...datum, t: `${datum.t} GMT` }))
-    }))
+    .then(parseJson)
+    .then(throwJsonErrors)
+    .then(mapTimeDataToGMT)
 }
 
 export const fetchPier17Data = () => {
